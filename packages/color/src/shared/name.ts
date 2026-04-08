876bbccc9e0b655b89209e57f983e3af0acfa85a
Coke <jock74608@gmail.com>
2026-04-08 17:40:48 +0800

    fix: bug fix

--- packages/color/src/shared/name.ts
+++ packages/color/src/shared/name.ts
@@ -11,39 +11,26 @@ export function getColorName(color: string) {
   const rgb = getRgb(color)
   const hsl = getHsl(color)
 
-  let ndf = 0
-  let ndf1 = 0
-  let ndf2 = 0
   let cl = -1
   let df = -1
 
-  let name = ""
-
-  colorNames.some((item, index) => {
-    const [hexValue, colorName] = item
-
-    const match = hex === hexValue
-
-    if (match) {
-      name = colorName
-    } else {
-      const { r, g, b } = getRgb(hexValue)
-      const { h, s, l } = getHsl(hexValue)
-
-      ndf1 = (rgb.r - r) ** 2 + (rgb.g - g) ** 2 + (rgb.b - b) ** 2
-      ndf2 = (hsl.h - h) ** 2 + (hsl.s - s) ** 2 + (hsl.l - l) ** 2
-
-      ndf = ndf1 + ndf2 * 2
-      if (df < 0 || df > ndf) {
-        df = ndf
-        cl = index
-      }
+  for (const [index, [hexValue, colorName]] of colorNames.entries()) {
+    if (hex === hexValue) {
+      return colorName
     }
 
-    return match
-  })
+    const { r, g, b } = getRgb(hexValue)
+    const { h, s, l } = getHsl(hexValue)
 
-  name = colorNames[cl][1]
+    const ndf1 = (rgb.r - r) ** 2 + (rgb.g - g) ** 2 + (rgb.b - b) ** 2
+    const ndf2 = (hsl.h - h) ** 2 + (hsl.s - s) ** 2 + (hsl.l - l) ** 2
+    const ndf = ndf1 + ndf2 * 2
 
-  return name
+    if (df < 0 || df > ndf) {
+      df = ndf
+      cl = index
+    }
+  }
+
+  return colorNames[cl][1]
 }
