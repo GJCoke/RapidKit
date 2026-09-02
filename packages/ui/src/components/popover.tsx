6cc2ac9f2da7e74c70fb52637d690eb083ffdb99
coke <jock74608@gmail.com>
2026-09-02 15:21:41 +0800

    feat(ui): add card, avatar, tooltip, popover, collapsible, skeleton, switch, breadcrumb

---
+++ packages/ui/src/components/popover.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+import * as PopoverPrimitive from "@radix-ui/react-popover"
+import { cn } from "../lib/utils"
+
+function Popover({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Root>) {
+  return <PopoverPrimitive.Root data-slot="popover" {...props} />
+}
+function PopoverTrigger({ ...props }: React.ComponentProps<typeof PopoverPrimitive.Trigger>) {
+  return <PopoverPrimitive.Trigger data-slot="popover-trigger" {...props} />
+}
+function PopoverContent({ className, align = "center", sideOffset = 4, ...props }: React.ComponentProps<typeof PopoverPrimitive.Content>) {
+  return (
+    <PopoverPrimitive.Portal>
+      <PopoverPrimitive.Content
+        data-slot="popover-content"
+        align={align}
+        sideOffset={sideOffset}
+        className={cn("z-50 w-72 rounded-md border border-border bg-popover p-1 text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 outline-none", className)}
+        {...props}
+      />
+    </PopoverPrimitive.Portal>
+  )
+}
+export { Popover, PopoverTrigger, PopoverContent }
